aa650c5398f4a2975d558d1fc04c736ea79200a1
Erin Blackmere <erinkb@gmail.com>
2016-04-02 18:42:23 -0400

    Mixing in a new trait: AppendedClues

--- standard-lib/src/test/scala/lib/LibTest.scala
+++ standard-lib/src/test/scala/lib/LibTest.scala
@@ -1,6 +1,6 @@
-package com.github.fellowship_of_the_bus.lib.test
+package com.github.fellowship_of_the_bus.lib
 
 import org.scalatest._
 
 /* Base class for Library tests */
-abstract class LibTest extends FunSuite with GivenWhenThen with Matchers { }
+abstract class LibTest extends FunSuite with GivenWhenThen with Matchers with AppendedClues { }
