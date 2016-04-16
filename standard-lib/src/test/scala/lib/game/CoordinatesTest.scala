39d6b15292d3ab802de454794cb91f8b0ee20bf1
Erin Blackmere <erinkb@gmail.com>
2016-04-16 19:05:33 -0400

    Adding outline for Coordinates test class

---
+++ standard-lib/src/test/scala/lib/game/CoordinatesTest.scala
@@ -0,0 +1,25 @@
+package com.github.fellowship_of_the_bus.lib
+package game
+
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+
+/* Test suite for lib/game/Coordinates.scala */
+@RunWith(classOf[JUnitRunner])
+class CoordinatesTest extends LibTest {
+  // TEST //
+  test("TEST: Verify full coordinates") (pending)
+
+  // TEST //
+  test("TEST: Verify top-left coordinates") (pending)
+
+  // TEST //
+  test("TEST: Verify bottom-right coordinates") (pending)
+
+  // TEST //
+  test("TEST: Verify center coordinates") (pending)
+
+  // TEST //
+  test("TEST: Verify distance function") (pending)
+}
